a55da9ec093372802d299873509cc69073b85d81
Raghu Betina <raghu@firstdraft.com>
2018-06-20 01:18:28 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "movies#index"
   # Routes for the Favorite resource:
   # CREATE
   get "/favorites/new", :controller => "favorites", :action => "new"
